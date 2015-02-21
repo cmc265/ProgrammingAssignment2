45d2754daf26401f2214924cf32da811d965f649
cmc265 <christopher.cochet@gmail.com>
2015-02-20 23:47:37 -0500

    Version 1 of the R programming assignement 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## makeCacheMatrix creates a special "matrix" with a list of function to
+  ##  - set the value of the vector
+  ##  - get the value of the vector
+  ##  - set the value of the mean
+  ##  - get the value of the mean
+  
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Returns a matrix that is the inverse of 'x'
+  
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  if(dim(data)[1]!=dim(data)[2]) {
+    return("Error: not a square matrix")
+  }
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
